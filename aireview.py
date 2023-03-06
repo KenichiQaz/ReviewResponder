db69d10375ebfe71ddce2a33dde6deefea499488
KenichiQaz <31177857+KenichiQaz@users.noreply.github.com>
2023-03-06 02:31:00 +0000

    Saving all reviews

--- aireview.py
+++ aireview.py
@@ -18,14 +18,15 @@ def review_gather():
     # Get all the google business reviews posted in the last hour
     url = f'{URL_PART1}placeid={PLACE_ID}&fields=reviews&key={API_KEY}'
     print(url)
-    payload={}
+    payload = {}
     headers = {}
 
-    response = requests.request("GET", url, headers=headers, data=payload, timeout=5)
+    response = requests.request(
+        "GET", url, headers=headers, data=payload, timeout=5)
     business_reviews = response.json()
     # # Loop through each review
     df = pd.json_normalize(business_reviews['result']['reviews'])
-    
+
     for review in business_reviews['result']['reviews']:
         # # Get the time the review was posted in milliseconds
         responsestr = ""
@@ -34,10 +35,14 @@ def review_gather():
         if review_time >= round(time.time()*1000) - 3600000:
             # convert review to dataframe and then use to_csv to save results in the csv file.
             responsestr = review_responder(review)
-        if responsestr != "":
-            df['response']=responsestr
+        if responsestr:
+            df['response'] = responsestr
         else:
-            df['response']=""
+            df['response'] = ""
+    if os.path.exists("reviews.csv"):
+        df.to_csv("reviews.csv", mode='a', index=True, header=False)
+    else:
+        df.to_csv("reviews.csv", mode='w', index=True, header=True)
 
 
 def review_responder(text):
