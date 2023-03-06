a5ab276c6f05f2e18bb28900c0536a85eb139f89
KenichiQaz <31177857+KenichiQaz@users.noreply.github.com>
2023-03-06 02:22:49 +0000

    added response col

--- aireview.py
+++ aireview.py
@@ -1,11 +1,14 @@
 '''   ~~~~~~   Google Review Responder   ~~~~~~   '''
 import os
-import datetime
+import time
 from google.cloud import language_v1 as lang
 import requests
+import pandas as pd
+import json
+from pandas import json_normalize
 
 API_KEY = os.environ.get("GOOGLEAPIKEY")
-PLACE_ID = "ChIJg5WqpxwodTERGZllG4JgTr0"  # AEON Mall Binh Duong Canary
+PLACE_ID = "ChIJe3V5nnIpdTERNWAyzBpURU4"  # Scotch AGS
 URL_PART1 = "https://maps.googleapis.com/maps/api/place/details/json?"
 
 
@@ -13,7 +16,7 @@ def review_gather():
     '''get reviews and pass them to the responder'''
 
     # Get all the google business reviews posted in the last hour
-    url = f'{URL_PART1}placeid={PLACE_ID}fields=reviews&key={API_KEY}'
+    url = f'{URL_PART1}placeid={PLACE_ID}&fields=reviews&key={API_KEY}'
     print(url)
     payload={}
     headers = {}
@@ -21,13 +24,20 @@ def review_gather():
     response = requests.request("GET", url, headers=headers, data=payload, timeout=5)
     business_reviews = response.json()
     # # Loop through each review
+    df = pd.json_normalize(business_reviews['result']['reviews'])
+    
     for review in business_reviews['result']['reviews']:
         # # Get the time the review was posted in milliseconds
+        responsestr = ""
         review_time = review['time']
     # If the review was posted in the last hour
-        if review_time >= (datetime.time() * 1000) - 3600000:
+        if review_time >= round(time.time()*1000) - 3600000:
             # convert review to dataframe and then use to_csv to save results in the csv file.
-            review_responder(review)
+            responsestr = review_responder(review)
+        if responsestr != "":
+            df['response']=responsestr
+        else:
+            df['response']=""
 
 
 def review_responder(text):
@@ -57,7 +67,7 @@ def review_responder(text):
         response = "Thank you for the kind words! We really appreciate your feedback."
     else:
         response = "We appreciate your feedback and we're always looking to improve our services."
-    return response
+    print(response)
 
 
 review_gather()
